47c9c7300789b78146754e8ff72d825796386fcd
Yuanchi MA <aclm234@163.com>
2018-11-18 16:14:58 +0800

    feat: finish all the features.

--- InstrumentBooking.py
+++ InstrumentBooking.py
@@ -1,10 +1,8 @@
 # -*- coding: UTF-8 -*-
 import re
 import requests
-#from skimage import io
 import base64
 import sys
-#import tesserocr
 import pytesseract
 from PIL import Image
 import http.cookiejar as cookielib
@@ -15,14 +13,15 @@ task_attribute = {}
 
 
 def main():
+	return 0
+
+def booking(value_instrument, value_year, value_month, value_day, value_start_time, value_stop_time, cycle_times = -1):
 	#下面是参数，cycle_times是尝试预约次数，-1为无限次数；instrument_name是预约仪器
-	cycle_times = -1
 	instrument_name = "BD FACS CantoII流式细胞分析仪"
 	session = requests.Session()
 	session = sign_in(session)
 	session1 = requests.Session()
-	#get_instrument_types(session)
-	book_result = booking_with_para(session, cycle_times, instrument_name)
+	book_result = booking_with_para(session, value_instrument, value_year, value_month, value_day, value_start_time, value_stop_time, cycle_times)
 	if book_result == "操作成功":
 		print("预约成功！")
 	else:
@@ -32,10 +31,8 @@ def main():
 def get_captcha(session):
 	CaptchaUrl = "http://10.1.7.222/yqgl/admin/CreateImage.aspx"
 	PostUrl = "http://10.1.7.222/yqgl/admin/default_yiqi.aspx"
-	#session = requests.session()
 	cookie = session.cookies
 	# 验证码地址和post地址
-	#cookie = cookielib.CookieJar()
 	handler = urllib2.HTTPCookieProcessor(cookie)
 	opener = urllib2.build_opener(handler)
 	# 将cookies绑定到一个opener cookie由cookielib自动管理
@@ -58,10 +55,8 @@ def get_captcha(session):
 
 def sign_in(session):
 	url_login = "http://10.1.7.222/yqgl/admin/default_yiqi.aspx"
-	#print(sys.getdefaultencoding())
 	
 	session, captcha_code = get_captcha(session)
-	#captcha = input("请输入：");
 	payload_login = {
 		'__VIEWSTATE':'/wEPDwUKMTAwMTQzNjkzOA9kFgICAw9kFgICDA8PFgQeBFRleHRlHgdWaXNpYmxlZ2RkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYDBQJtMQUCbTMFAm0yoNlCc2s5wYvDBhhiVwwEnaav4EI=',
 		'ename_b':'wenyuqi',
@@ -75,9 +70,7 @@ def sign_in(session):
 	headers_login = {}
 
 	contain = session.post(url_login, payload_login, headers_login)
-	#print(str(contain.content).replace('\\r\\n', '\n'))
 	gb_contain = contain.content.decode("gb2312","ignore")
-	#print(gb_contain)
 	if '<script language=Javascript>alert' in gb_contain:
 		print('There is a mistake during signing in.\n Trying again...\n')
 		time.sleep(2)
@@ -85,18 +78,8 @@ def sign_in(session):
 	return session
 
 
-def get_instrument_types(session):
-	url_types = "http://10.1.7.222/yqgl/admin/apparatus/return1.aspx?lei=1"
-	headers_types = {}
-	contain = session.get(url_types)
-	gb_contain = contain.content.decode("gb2312","ignore")
-	#print(gb_contain)
-
-
-def booking_with_para(session, cycle_times = -1, instrument_name = "BD FACS CantoII流式细胞分析仪"):
+def booking_with_para(session, value_instrument, value_year, value_month, value_day, value_start_time, value_stop_time, cycle_times = -1):
 	status = 'cycle'
-	#cycle_times = -1
-	#instrument_name = "BD FACS CantoII流式细胞分析仪"#Bioruptor plus超声破碎仪 BD FACS CantoII流式细胞分析仪
 	instrument_id = {
 		'BD FACS CantoII流式细胞分析仪':'20130403A0001',
 		'BD Influx流式细胞分选仪':'20111229A0004',
@@ -107,26 +90,21 @@ def booking_with_para(session, cycle_times = -1, instrument_name = "BD FACS Cant
 		'old BD FACS AriaIII流式细胞分选仪':'20111229A0003',
 		'Bioruptor plus超声破碎仪':'20150715A0001'
 	}
-	url_book = "http://10.1.7.222/yqgl/admin/apparatus/add4.aspx?id=" + instrument_id[instrument_name]
+	url_book = "http://10.1.7.222/yqgl/admin/apparatus/add4.aspx?id=" + instrument_id[value_instrument]
 	contain = session.get(url_book)
 	gb_contain = contain.content.decode("gb2312","ignore")
-	#print(gb_contain)
-	print(url_book)
 	EVENTVALIDATION_value = gb_contain.split('<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="')[1]\
 		.split('" />')[0]
 	VIEWSTATE_value = gb_contain.split('<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="')[1]\
 		.split('" />')[0]
-	#print('qqqqqqqq\n' + EVENTVALIDATION_value + '\n\nppppppppp\n\n\n')
-	#print('qqqqqqqq\n' + VIEWSTATE_value + '\n\nppppppppp')
-	#return 0
 	payload_book = {
 		'__EVENTTARGET':'',
 		'__EVENTARGUMENT':'',
 		'__LASTFOCUS':'',
 		'__VIEWSTATE':VIEWSTATE_value,
-		'apparatus_date_id_b':instrument_id[instrument_name] + '#2018-11-16',#todo
-		'hour1_b':'16:30',#todo
-		'hour2_b':'19:30',#todo
+		'apparatus_date_id_b':instrument_id[value_instrument] + '#' + value_year + '-' + value_month + '-' + value_day,
+		'hour1_b':value_start_time,
+		'hour2_b': value_stop_time,
 		'teac_name_b':'1',
 		'title1_b':'1',
 		'title2_b':'1',
@@ -139,53 +117,22 @@ def booking_with_para(session, cycle_times = -1, instrument_name = "BD FACS Cant
 		'Button1':'(unable to decode value)',
 		'__EVENTVALIDATION':EVENTVALIDATION_value,
 	}
-	#print(payload_book)
 	headers_book = {}
 
 	contain = session.post(url_book, payload_book, headers_book)
-	#print(str(contain.content).replace('\\r\\n', '\n'))
 	gb_contain = contain.content.decode("gb2312","ignore")
-	#print(gb_contain)
-	book_result = gb_contain.split("<script language=Javascript>alert('")[1]\
-		.split("');")[0]
+	try:
+		book_result = gb_contain.split("<script language=Javascript>alert('")[1]\
+			.split("');")[0]
+	except:
+		book_result = '未到时间'
 	print('result is: ' + book_result + '\n')
 	if cycle_times != -1:
 		cycle_times -= 1
 	if book_result != "操作成功" and cycle_times != 0:
-		if cycle_times == -1:
-			print("无限循环预定")
-		print("剩余循环次数：" + str(cycle_times))
-		print('\nTrying booking again...')
-		time.sleep(0.2)
-		booking_with_para(session, cycle_times)
+		booking_with_para(session, value_instrument, value_year, value_month, value_day, value_start_time, value_stop_time, cycle_times)
 	return session, book_result
 
-	
-'''
-def parse_capt(image_url):
-	# 获取这个图片的内容
-	response = requests.get(image_url)
-
-	# 获取base64的str
-	base64_str = base64.b64encode(response.content)
-	v_type = 'cn'
-	# post提交打码平台的数据
-	form = {
-		'v_pic': base64_str,
-		'v_type': v_type,
-	}
-	# 打码平台 Authorization的header
-	headers = {
-		'Authorization': 'APPCODE 926e3a416dd34ef0be35a19809ade4c9',
-	}
-	# 从打码平台获取验证码信息
-	dmpt_url = 'http://yzmplus.market.alicloudapi.com/fzyzm'
-	response = requests.post(dmpt_url, form, headers=headers)
-	print(response.text)
-	# captcha_value 就是我们的验证码信息
-	captcha_value = response.json()['v_code']
-	print(captcha_value)
-'''
 
 if __name__ == '__main__':
 	main()
--- gui.py
+++ gui.py
@@ -1,7 +1,22 @@
 # -*- coding: UTF-8 -*-
  
 from tkinter import *
+from InstrumentBooking import *
 from tkinter import ttk
+import time
+import threading
+
+threads = []
+booking_flag = False
+circle_times = 0
+value_input = {
+		'instrument': '',
+		'year': '',
+		'month': '',
+		'day': '',
+		'start_time': '',
+		'stop_time': '',
+		}
 
 #选择仪器
 def creat_instrument_selection(root):
@@ -63,7 +78,7 @@ def creat_date_selection(root):
 	value_month = StringVar()
 	comboxlist_month = ttk.Combobox(frame_date_selection, textvariable=value_month)
 	#下拉框可选值&默认值
-	comboxlist_month["values"] = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',)
+	comboxlist_month["values"] = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',)
 	comboxlist_month.current(0)
 	comboxlist_month['state'] = 'readonly'
 	#下拉框位置&尺寸
@@ -74,7 +89,11 @@ def creat_date_selection(root):
 	value_day = StringVar()
 	comboxlist_day = ttk.Combobox(frame_date_selection, textvariable=value_day)
 	#下拉框可选值&默认值
-	comboxlist_day["values"] = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', )
+	comboxlist_day["values"] = (
+			'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', 
+			'11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 
+			'21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', 
+		)
 	comboxlist_day.current(0)
 	comboxlist_day['state'] = 'readonly'
 	#下拉框位置&尺寸
@@ -83,30 +102,151 @@ def creat_date_selection(root):
 	return root, value_year, value_month, value_day
 	
 
+#选择时间
+def creat_time_selection(root):
+	frame_time_selection = Frame(root, height=80,width=100,bd=5)#, bg='#ff3399')
+	frame_time_selection.pack(anchor=W) 
+	#选择开始时间示意文字：
+	lb_start_time_name = Label(frame_time_selection, text = "开始时间：")
+	lb_start_time_name.grid(column = 0, row = 0, padx = 10, pady = 4)
+	#设置下拉框（选择开始时间）
+	value_start_time = StringVar()
+	comboxlist_start_time = ttk.Combobox(frame_time_selection, textvariable=value_start_time)
+	#下拉框可选值&默认值
+	time_value = (
+			'08:30',
+			'09:00', '09:30',
+			'10:00', '10:30',
+			'11:00', '11:30',
+			'12:00', '12:30',
+			'13:00', '13:30',
+			'14:00', '14:30',
+			'15:00', '15:30',
+			'16:00', '16:30',
+			'17:00', '17:30',
+			'18:00', '18:30',
+			'19:00', '19:30',
+			'20:00', '20:30',
+			'21:00', '21:30',
+			'22:00', '22:30',
+			'23:00', '23:30',
+		)
+	comboxlist_start_time["values"] = time_value
+	comboxlist_start_time.current(0)
+	comboxlist_start_time['state'] = 'readonly'
+	#下拉框位置&尺寸
+	comboxlist_start_time.grid(column=1, row=0)
+	comboxlist_start_time['width'] = 50
+	
+	#选择结束时间示意文字：
+	lb_stop_time_name = Label(frame_time_selection, text = "结束时间：")
+	lb_stop_time_name.grid(column = 0, row = 1, padx = 10, pady = 4)
+	#设置下拉框（选择结束时间）
+	value_stop_time = StringVar()
+	comboxlist_stop_time = ttk.Combobox(frame_time_selection, textvariable=value_stop_time)
+	comboxlist_stop_time["values"] = time_value
+	comboxlist_stop_time.current(6)
+	comboxlist_stop_time['state'] = 'readonly'
+	#下拉框位置&尺寸
+	comboxlist_stop_time.grid(column=1, row=1)
+	comboxlist_stop_time['width'] = 50
+	
+	return root, value_start_time, value_stop_time
 
-def main():
-	root = Tk()
+
+#选择模式
+def creat_mode_selection(root):
+	frame_mode_selection = Frame(root, height=80,width=100,bd=5)#, bg='#ff3399')
+	frame_mode_selection.pack(anchor=W) 
+	#选择仪器示意文字：
+	lb_mode_name = Label(frame_mode_selection, text = "预定模式：")
+	lb_mode_name.grid(column = 0, row = 0, padx = 10, pady = 4)
+	#设置下拉框（选择仪器）
+	value_mode = StringVar()
+	comboxlist_mode = ttk.Combobox(frame_mode_selection, textvariable=value_mode)
+	#下拉框可选值&默认值
+	comboxlist_mode["values"] = ('持续预定', '单次预定')
+	comboxlist_mode.current(0)
+	comboxlist_mode['state'] = 'readonly'
+	#下拉框位置&尺寸
+	comboxlist_mode.grid(column=1, row=0)
+	comboxlist_mode['width'] = 10
+	return root, value_mode
+
+	
+def gui_main():
+	root = Tk() 
 	#设置窗口的大小宽x高+偏移量
 	root.geometry('500x300+500+200')
 	#窗口标题
 	root.title("InstrumentBooking")
 	#设置窗口图标
 	root.iconbitmap('pic/Cbi.ico')
+	
+	#创建选择下拉框界面
 	root, value_instrument = creat_instrument_selection(root)
 	root, value_year, value_month, value_day = creat_date_selection(root)
-	def rtnkey(event=None):
-		print(e.get())
-		print(value_instrument.get())
-		print(value_year.get())
-		print(value_month.get())
-		print(value_day.get())
-	e = StringVar()
-	entry = Entry(root, validate='key', textvariable=e, width=50)
-	entry.pack()
-	entry.bind('<Return>', rtnkey)
-	# 进入消息循环
+	root, value_start_time, value_stop_time = creat_time_selection(root)
+	root, value_mode = creat_mode_selection(root)
+	
+	def confirm_event():
+		global booking_flag, circle_times, value_input
+		print('\n\n------------------------')
+		booking_flag = True
+		value_input['instrument'] = value_instrument.get()
+		value_input['year'] = value_year.get()
+		value_input['month'] = value_month.get()
+		value_input['day'] = value_day.get()
+		value_input['start_time'] = value_start_time.get()
+		value_input['stop_time'] = value_stop_time.get()
+		print(value_input)
+		if value_mode.get() == '持续预定':
+			print('持续预定')
+			circle_times = -1
+		else:
+			print('单次预定')
+			circle_times = 1
+		
+	def cancel_event():
+		global booking_flag
+		print('\nCancel mission...')
+		booking_flag = False
+	confirm_button = Button(root,text='Confirm',command=confirm_event, width=10)
+	confirm_button.pack()
+	cancel_button = Button(root,text='Cancel',command=cancel_event, width=10)
+	cancel_button.pack()
 	root.mainloop()
 	return 0
+
+
+def book_api():
+	print('start book_api')
+	global booking_flag, circle_times, value_input
+	while True:
+		time.sleep(1)
+		if booking_flag:
+			booking(value_input['instrument'], value_input['year'], value_input['month'], value_input['day'], value_input['start_time'], value_input['stop_time'], 1)
+			if circle_times == 1:
+				booking_flag = False
+			else:
+				print('\nTrying booking again...')
+				time.sleep(0.2)
+	print('stop book_api')
+
+
+t1 = threading.Thread(target=book_api,args=())
+threads.append(t1)
+
+
+def main():
+	global threads
+	for t in threads:
+		print(t)
+		t.setDaemon(True)
+		t.start()
+	gui_main()
+	
+
 
 if __name__ == '__main__':
 	main()
